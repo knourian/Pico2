2e3d2a14f29c9ad29b2cb44a36d58aa3048982ed
Khashayar Nourian <knourian@users.noreply.github.com>
2018-05-30 18:29:14 +0430

    Init Commit

---
+++ config.py
@@ -0,0 +1,114 @@
+import logging
+
+########################################################################
+#
+#   General options
+
+### Logging
+log_level = logging.INFO
+log_format = '%(asctime)s %(levelname)s %(name)s: %(message)s'
+
+### Server
+listening_ip = "127.0.0.1"
+listening_port = 8080
+
+### Cost Estimate
+kwh_rate = 0.26  # Rate in currency_type to calculate cost to run job
+currency_type = "EUR"  # Currency Symbol to show when calculating cost to run job
+
+########################################################################
+#
+#   GPIO Setup (BCM SoC Numbering Schema)
+#
+#   Check the RasPi docs to see where these GPIOs are
+#   connected on the P1 header for your board type/rev.
+#   These were tested on a Pi B Rev2 but of course you
+#   can use whichever GPIO you prefer/have available.
+
+### Outputs
+#gpio_heat = 12  # Switches zero-cross solid-state-relay
+gpio_cool = 16  # Regulates PWM for 12V DC Blower
+gpio_air = 20  # Switches 0-phase det. solid-state-relay
+
+
+def get_gpio_sensor_data(channel):
+    if channel == 1:
+        return 26
+    if channel == 2:
+        return 24
+    if channel == 3:
+        return 22
+    if channel == 4:
+        return 28
+    if channel == 5:
+        return 27
+    if channel == 6:
+        return 25
+
+
+def get_gpio_heat(channel):
+    if channel == 1:
+        return 12
+    if channel == 2:
+        return 14
+    if channel == 3:
+        return 16
+    if channel == 4:
+        return 18
+    if channel == 5:
+        return 11
+    if channel == 6:
+        return 13
+
+heater_invert = 0  # switches the polarity of the heater control
+Data_path=""
+### Inputs
+gpio_door = 6
+
+### Thermocouple Adapter selection:
+#   max31855 - bitbang SPI interface
+#   max31855spi - kernel SPI interface
+#   max6675 - bitbang SPI interface
+max31855 = 0
+max6675 = 1
+max31855spi = 0  # if you use this one, you MUST reassign the default GPIO pins
+
+### Thermocouple Connection (using bitbang interfaces)
+gpio_sensor_cs = 13
+gpio_sensor_clock = 19
+#gpio_sensor_data = 24
+
+### Thermocouple SPI Connection (using adafrut drivers + kernel SPI interface)
+spi_sensor_chip_id = 0
+
+### amount of time, in seconds, to wait between reads of the thermocouple
+sensor_time_wait = .5
+
+########################################################################
+#
+#   PID parameters
+
+pid_ki = 0.1  # Integration
+pid_kd = 0.4  # Derivative
+pid_kp = 0.5  # Proportional
+
+########################################################################
+#
+#   Simulation parameters
+
+sim_t_env = 25.0  # deg C
+sim_c_heat = 100.0  # J/K  heat capacity of heat element
+sim_c_oven = 2000.0  # J/K  heat capacity of oven
+sim_p_heat = 3500.0  # W    heating power of oven
+sim_R_o_nocool = 1.0  # K/W  thermal resistance oven -> environment
+sim_R_o_cool = 0.05  # K/W  " with cooling
+sim_R_ho_noair = 0.1  # K/W  thermal resistance heat element -> oven
+sim_R_ho_air = 0.05  # K/W  " with internal air circulation
+
+########################################################################
+#
+#   Time and Temperature parameters
+
+temp_scale = "c"  # c = Celsius | f = Fahrenheit - Unit to display
+time_scale_slope = "s"  # s = Seconds | m = Minutes | h = Hours - Slope displayed in temp_scale per time_scale_slope
+time_scale_profile = "s"  # s = Seconds | m = Minutes | h = Hours - Enter and view target time in time_scale_profile
---
+++ picoreflowd.py
@@ -0,0 +1,332 @@
+#!/usr/bin/env python2
+
+import os
+import profile
+import sys
+import logging
+import json
+
+import bottle
+import gevent
+import geventwebsocket
+from gevent.pywsgi import WSGIServer
+from geventwebsocket import WebSocketError
+from geventwebsocket.handler import WebSocketHandler
+
+try:
+    sys.dont_write_bytecode = True
+    import config
+
+    sys.dont_write_bytecode = False
+except:
+    print ("Could not import config file.")
+    print ("Copy config.py.EXAMPLE to config.py and adapt it for your setup.")
+    exit(1)
+
+logging.basicConfig(level=config.log_level, format=config.log_format)
+log = logging.getLogger("picoreflowd")
+log.info("Starting picoreflowd")
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+log.info("script_dir" + script_dir)
+sys.path.insert(0, script_dir + '/lib/')
+profile_path = os.path.join(script_dir, "storage", "profile")
+
+Data_path = os.path.join(script_dir, "storage", "Data")
+
+from lib.oven import Oven, Profile
+from lib.ovenWatcher import OvenWatcher
+config.Data_path=Data_path
+app = bottle.Bottle()
+oven = Oven()
+ovenWatcher = OvenWatcher(oven)
+
+
+@app.route('/')
+def index():
+    return bottle.redirect('/picoreflow/indexnew.html')
+
+
+@app.route('/picoreflow/:filename#.*#')
+def send_static(filename):
+    log.debug("serving %s" % filename)
+    return bottle.static_file(filename, root=os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), "public"))
+
+
+def get_websocket_from_request():
+    env = bottle.request.environ
+    wsock = env.get('wsgi.websocket')
+    if not wsock:
+        bottle.abort(400, 'Expected WebSocket request.')
+    return wsock
+
+
+@app.route('/control')
+def handle_control():
+    wsock = get_websocket_from_request()
+    log.info("websocket (control) opened")
+    while True:
+        try:
+            message = wsock.receive()
+            log.info("Received (control): %s" % message)
+            msgdict = json.loads(message)
+            if msgdict.get("cmd") == "RUN":
+                log.info("RUN command received")
+                profile_obj = msgdict.get('profile')
+                channel = 1
+                if profile_obj:
+                    if eval(((profile_obj['channel'][0]['activated']) + "").capitalize()):
+                        profile_json = json.dumps(profile_obj['channel'][0]['profile'])
+                        profile = Profile(profile_json)
+                        channel = 1
+                        simulated_oven = Oven(channel, simulate=True, time_step=0.5)
+                        simulation_watcher = OvenWatcher(simulated_oven)
+                        simulation_watcher.add_observer(wsock)
+                        simulated_oven.run_profile(profile)
+                        simulation_watcher.record(profile)
+                    if eval(((profile_obj['channel'][1]['activated']) + "").capitalize()):
+                        profile_json = json.dumps(profile_obj['channel'][1]['profile'])
+                        profile = Profile(profile_json)
+                        channel = 2
+                        simulated_oven = Oven(channel, simulate=True, time_step=0.5)
+                        simulation_watcher = OvenWatcher(simulated_oven)
+                        simulation_watcher.add_observer(wsock)
+                        simulated_oven.run_profile(profile)
+                        simulation_watcher.record(profile)
+                    if eval(((profile_obj['channel'][2]['activated']) + "").capitalize()):
+                        profile_json = json.dumps(profile_obj['channel'][2]['profile'])
+                        profile = Profile(profile_json)
+                        channel = 3
+                        simulated_oven = Oven(channel, simulate=True, time_step=0.5)
+                        simulation_watcher = OvenWatcher(simulated_oven)
+                        simulation_watcher.add_observer(wsock)
+                        simulated_oven.run_profile(profile)
+                        simulation_watcher.record(profile)
+                    if eval(((profile_obj['channel'][3]['activated']) + "").capitalize()):
+                        profile_json = json.dumps(profile_obj['channel'][3]['profile'])
+                        profile = Profile(profile_json)
+                        channel = 4
+                        simulated_oven = Oven(channel, simulate=True, time_step=0.5)
+                        simulation_watcher = OvenWatcher(simulated_oven)
+                        simulation_watcher.add_observer(wsock)
+                        simulated_oven.run_profile(profile)
+                        simulation_watcher.record(profile)
+                    if eval(((profile_obj['channel'][4]['activated']) + "").capitalize()):
+                        profile_json = json.dumps(profile_obj['channel'][4]['profile'])
+                        profile = Profile(profile_json)
+                        channel = 5
+                        simulated_oven = Oven(channel, simulate=True, time_step=0.5)
+                        simulation_watcher = OvenWatcher(simulated_oven)
+                        simulation_watcher.add_observer(wsock)
+                        simulated_oven.run_profile(profile)
+                        simulation_watcher.record(profile)
+                    if eval(((profile_obj['channel'][5]['activated']) + "").capitalize()):
+                        profile_json = json.dumps(profile_obj['channel'][5]['profile'])
+                        profile = Profile(profile_json)
+                        channel = 6
+                        simulated_oven = Oven(channel, simulate=True, time_step=0.5)
+                        simulation_watcher = OvenWatcher(simulated_oven)
+                        simulation_watcher.add_observer(wsock)
+                        simulated_oven.run_profile(profile)
+                        simulation_watcher.record(profile)
+                # log.info("profile Sent: ")
+                # log.info(profile.name)
+                # log.info("channel   : ")
+                # log.info(channel)
+            # elif msgdict.get("cmd") == "SIMULATE":
+            #     log.info("SIMULATE command received")
+            #     profile_obj = msgdict.get('profile')
+            #     if profile_obj:
+            #         profile_json = json.dumps(profile_obj)
+            #         profile = Profile(profile_json)
+            #     simulated_oven = Oven(simulate=True, time_step=0.5)
+            #     simulation_watcher = OvenWatcher(simulated_oven)
+            #     simulation_watcher.add_observer(wsock)
+            #     # simulated_oven.run_profile(profile)
+            #     # simulation_watcher.record(profile)
+            elif msgdict.get("cmd") == "STOP":
+                log.info("Stop command received")
+                oven.abort_run()
+                simulated_oven.abort_run();
+        except WebSocketError:
+            break
+    log.info("websocket (control) closed")
+
+
+@app.route('/storage')
+def handle_storage():
+    wsock = get_websocket_from_request()
+    log.info("websocket (storage) opened")
+    while True:
+        try:
+            message = wsock.receive()
+            if not message:
+                break
+            log.debug("websocket (storage) received: %s" % message)
+
+            try:
+                msgdict = json.loads(message)
+            except:
+                msgdict = {}
+
+            if message == "GET":
+                log.info("GET command recived")
+                wsock.send(get_profiles())
+            elif msgdict.get("cmd") == "DELETE":
+                log.info("DELETE command received")
+                profile_obj = msgdict.get('profile')
+                if delete_profile(profile_obj):
+                    msgdict["resp"] = "OK"
+                wsock.send(json.dumps(msgdict))
+                # wsock.send(get_profiles())
+            elif msgdict.get("cmd") == "PUT":
+                log.info("PUT command received")
+                profile_obj = msgdict.get('profile')
+                force = msgdict.get('force', False)
+                if profile_obj:
+                    # del msgdict["cmd"]
+                    if save_profile(profile_obj, force):
+                        msgdict["resp"] = "OK"
+                    else:
+                        msgdict["resp"] = "FAIL"
+                    log.debug("websocket (storage) sent: %s" % message)
+                    wsock.send(json.dumps(msgdict))
+                    wsock.send(get_profiles())
+            elif msgdict.get("cmd") == "SAVE":
+                log.info("SAVE command received")
+                profile_obj = msgdict.get('channels')
+                if profile_obj:
+                    if save_channel(profile_obj, True):
+                        msgdict["resp"] = "OK"
+                    else:
+                        msgdict["resp"] = "FAIL"
+                    log.debug("websocket (storage) sent: %s" % message)
+                    wsock.send(json.dumps(msgdict))
+                    wsock.send(get_channel())
+            elif message == "GETCH":
+                log.info("GETCH command recived")
+                wsock.send(get_channel())
+        except WebSocketError:
+            break
+    log.info("websocket (storage) closed")
+
+
+@app.route('/config')
+def handle_config():
+    wsock = get_websocket_from_request()
+    log.info("websocket (config) opened")
+    while True:
+        try:
+            message = wsock.receive()
+            if not message:
+                break
+            log.debug("websocket (storage) received: %s" % message)
+            try:
+                msgdict = json.loads(message)
+            except:
+                msgdict = {}
+            wsock.send(get_config())
+        except WebSocketError:
+            break
+    log.info("websocket (config) closed")
+
+
+@app.route('/status')
+def handle_status():
+    wsock = get_websocket_from_request()
+    ovenWatcher.add_observer(wsock)
+    log.info("websocket (status) opened")
+    while True:
+        try:
+            message = wsock.receive()
+            wsock.send("Your message was: %r" % message)
+        except WebSocketError:
+            break
+    log.info("websocket (status) closed")
+
+
+def get_profiles():
+    try:
+        profile_files = os.listdir(profile_path)
+    except:
+        profile_files = []
+    profiles = []
+    for filename in profile_files:
+        with open(os.path.join(profile_path, filename), 'r') as f:
+            profiles.append(json.load(f))
+
+    return json.dumps(profiles)
+
+
+def get_channel():
+    try:
+        Channel_path = os.path.join(script_dir, "public");
+        Channel_file = os.listdir(Channel_path)
+    except:
+        Channel_file = []
+    channels = []
+    for filename in Channel_file:
+        if filename == "channel.json":
+            with open(os.path.join(Channel_path, filename), 'r') as f:
+                channels = json.load(f)
+
+    return json.dumps(channels)
+
+
+def save_profile(profile, force=False):
+    profile_json = json.dumps(profile)
+    filename = profile['name'] + ".json"
+    filepath = os.path.join(profile_path, filename)
+    if not force and os.path.exists(filepath):
+        log.error("Could not write, %s already exists" % filepath)
+        return False
+    with open(filepath, 'w+') as f:
+        f.write(profile_json)
+        f.close()
+    log.info("Wrote %s" % filepath)
+    return True
+
+
+def save_channel(channel, force=False):
+    profile_json = json.dumps(channel)
+    filename = "channel" + ".json"
+    filepath = os.path.join(script_dir, "public", filename)
+    if not force and os.path.exists(filepath):
+        log.error("Could not write, %s already exists" % filepath)
+        return False
+    with open(filepath, 'w+') as f:
+        f.write(profile_json)
+        f.close()
+    log.info("Wrote %s" % filepath)
+    return True
+
+
+def delete_profile(profile):
+    profile_json = json.dumps(profile)
+    filename = profile['name'] + ".json"
+    filepath = os.path.join(profile_path, filename)
+    os.remove(filepath)
+    log.info("Deleted %s" % filepath)
+    return True
+
+
+def get_config():
+    return json.dumps({"temp_scale": config.temp_scale,
+                       "time_scale_slope": config.time_scale_slope,
+                       "time_scale_profile": config.time_scale_profile,
+                       "kwh_rate": config.kwh_rate,
+                       "currency_type": config.currency_type})
+
+
+def main():
+    ip = config.listening_ip
+    port = config.listening_port
+    log.info("listening on %s:%d" % (ip, port))
+
+    server = WSGIServer((ip, port), app,
+                        handler_class=WebSocketHandler)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
